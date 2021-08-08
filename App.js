9b7b0ca96418d81670c852228e911d0ea6f0e92c
Duraisamy303 <60089483+Duraisamy303@users.noreply.github.com>
2021-08-08 13:04:25 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Home from './E-Commerce-App/pages/Home/Home.js'
+
+export default function App(props) {
+    
+
+    return (
+        <>
+            <div><Home /></div>
+        </>
+    )
+}
+
